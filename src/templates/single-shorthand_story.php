cc1de032cadbbcf1e77b8612d0e65bc2d93f15e7
David Darke <david@atomicsmash.co.uk>
2018-08-07 09:05:41 +0100

    Added basic password form support to the 'single-shorthand_story' template and removed some of the opening and closing php tags.

--- src/templates/single-shorthand_story.php
+++ src/templates/single-shorthand_story.php
@@ -1,22 +1,29 @@
-<?php get_header(); ?>
+<?php get_header();
 
-	<?php
-	while ( have_posts() ) : the_post();
-		$meta = get_post_meta($post->ID);
-	?>
+	// Check to see if there is a password set against the post
+	if ( post_password_required( $post->ID ) ) {
 
-	<?php echo trim($meta['story_body'][0]); ?>
+		 echo get_the_password_form();
 
-	<div id="extraHTML">
-		<?php echo trim($meta['extra_html'][0]); ?>
-	</div>
+	} else {
 
-	<style type="text/css">
-		<?php echo get_option('sh_css'); ?>
-	</style>
+		while ( have_posts() ) : the_post();
+			$meta = get_post_meta($post->ID);
+		?>
 
-	<?php
-	endwhile;
-	?>
+		<?php echo trim($meta['story_body'][0]); ?>
 
-<?php get_footer(); ?>
+		<div id="extraHTML">
+			<?php echo trim($meta['extra_html'][0]); ?>
+		</div>
+
+		<style type="text/css">
+			<?php echo get_option('sh_css'); ?>
+		</style>
+
+		<?php
+		endwhile;
+
+	}
+
+get_footer();
