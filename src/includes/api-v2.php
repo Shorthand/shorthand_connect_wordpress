defed8da5e1fc70248164f1c1e62cb68d22127c3
Andrew Cobby <andrew.cobby@gmail.com>
2020-05-21 11:21:24 +1000

    Refactor Shorthand V2 API Interactions
    Pulls all the common stuff out of each function into some utility
    functions.

--- src/includes/api-v2.php
+++ src/includes/api-v2.php
@@ -2,74 +2,71 @@
 
 // VERSION 2 API
 
-function sh_get_profile() {
-
+function sh_v2_api_get($url, $options) {
 	global $serverv2URL;
 	$token = get_option('sh_v2_token');
 
-	$tokeninfo = array();
+	if (!$token) {
+		return false;
+	}
 
-	if($token) {
-		$url = $serverv2URL.'/v2/token-info';
-		$response = wp_remote_get( $url, array(
-			headers => array(
+	$url = $serverv2URL.$url;
+	$request_options = array_merge(
+		array(
+			'headers' => array(
 				'Authorization' => 'Token '.$token,
 				'user-agent'  =>  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8) AppleWebKit/535.6.2 (KHTML, like Gecko) Version/5.2 Safari/535.6.2',
 			)
-		) );
-		$body = wp_remote_retrieve_body( $response );
-		$data = json_decode($body);
-		if(isset($data) && isset($data->name)) {
-			$tokeninfo['username'] = $data->name . ' ('.$data->token_type.' Token)';
-			$tokeninfo['gravatar'] = $data->logo;
-			$tokeninfo = (object)$tokeninfo;
-		}
+		),
+		$options
+	);
+
+	return wp_remote_get($url, $request_options);
+}
+
+function sh_v2_api_get_json($url, $options) {
+	$response = sh_v2_api_get($url, $options);
+	$body = wp_remote_retrieve_body($response);
+	return json_decode($body);
+}
+
+function sh_get_profile() {
+	$tokeninfo = array();
+
+	$data = sh_v2_api_get_json('/v2/token-info', array());
+	if ($data && isset($data->name)) {
+		$tokeninfo['username'] = $data->name . ' ('.$data->token_type.' Token)';
+		$tokeninfo['gravatar'] = $data->logo;
+		$tokeninfo = (object)$tokeninfo;
 	}
 
 	return $tokeninfo;
 }
 
 function sh_get_stories() {
-	global $serverv2URL;
-	$token = get_option('sh_v2_token');
-
-	$valid_token = true;
-
 	$stories = null;
 
-	//Attempt to connect to the server
-	if($token) {
-		$url = $serverv2URL.'/v2/stories';
-		$response = wp_remote_get( $url, array(
-			'headers' => array(
-				'Authorization' => 'Token '.$token,
-				'user-agent'  =>  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8) AppleWebKit/535.6.2 (KHTML, like Gecko) Version/5.2 Safari/535.6.2',
-			),
-			'timeout' => '240'
-		) );
-		$body = wp_remote_retrieve_body( $response );
-		$data = json_decode($body);
-		if(isset($data)) {
-			$stories = array();
-			$valid_token = true;
-			//Something went wrong
-			if ($data->status) {
-				return null;
-			}
-			foreach($data as $storydata) {
-				$story = array(
-					'image' => $storydata->cover,
-					'id' => $storydata->id,
-					'metadata' => (object)array(
-						'description' => $storydata->description
-					),
-					'title' => $storydata->title,
-					'story_version' => ''.$storydata->version
-				);
-				$stories[] = (object)$story;
-			}
+	$data = sh_v2_api_get_json('/v2/stories', array('timeout' => '240'));
+	if($data) {
+		$stories = array();
+		//Something went wrong
+		if ($data->status) {
+			return null;
+		}
+		foreach($data as $storydata) {
+			$story = array(
+				'image' => $storydata->cover,
+				'id' => $storydata->id,
+				'metadata' => (object)array(
+					'description' => $storydata->description
+				),
+				'title' => $storydata->title,
+				'story_version' => ''.$storydata->version
+			);
+			$stories[] = (object)$story;
 		}
 	}
+	
 	return $stories;
 }
 
@@ -101,46 +98,34 @@ function sh_copy_story($post_id, $story_id) {
 	$tmpdir = get_temp_dir();
 	$destination_path = $destination['path'].'/shorthand/'.$post_id;
 
-	global $serverv2URL;
-	$token = get_option('sh_v2_token');
-
-	$valid_token = false;
-
 	$story = array();
 
 	//Attempt to connect to the server
-	if($token) {
-		$url = $serverv2URL.'/v2/stories/'.$story_id;
-		$zipfile = tempnam($tmpdir, 'sh_zip');
-		$unzipdir = tempnam($tmpdir, 'sh_unzip').'_dir';
-		$response = wp_remote_get( $url, array(
-			'headers' => array(
-				'Authorization' => 'Token '.$token,
-				'user-agent'  =>  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8) AppleWebKit/535.6.2 (KHTML, like Gecko) Version/5.2 Safari/535.6.2',
-			),
-			'timeout' => '600',
-			'stream' => true,
-			'filename' => $zipfile
-		) );
-		if($response['response']['code'] == 200) {
-			$unzipfile = unzip_file( $zipfile, $unzipdir);
-				if ( $unzipfile == 1 ) {
-					wp_mkdir_p($destination_path.'/'.$story_id);
-					$err = copy_dir($unzipdir, $destination_path.'/'.$story_id);
-					$story['path'] = $destination_path.'/'.$story_id;
-				} else {
-					$story['error'] = array(
+	$zipfile = tempnam($tmpdir, 'sh_zip');
+	$unzipdir = tempnam($tmpdir, 'sh_unzip').'_dir';
+	$response = sh_v2_api_get('/v2/stories/'.$story_id, array(
+		'timeout' => '600',
+		'stream' => true,
+		'filename' => $zipfile
+	));
+	if($response['response']['code'] == 200) {
+		$unzipfile = unzip_file( $zipfile, $unzipdir);
+			if ( $unzipfile == 1 ) {
+				wp_mkdir_p($destination_path.'/'.$story_id);
+				$err = copy_dir($unzipdir, $destination_path.'/'.$story_id);
+				$story['path'] = $destination_path.'/'.$story_id;
+			} else {
+				$story['error'] = array(
 					'pretty' => 'Could not unzip file'
 				);
-				}
-		} else {
-			$story['error'] = array(
-				'pretty' => 'Could not get zip file',
-				'error' => curl_error($ch),
-				'response' => $response
-			);
-		}
+			}
+	} else {
+		$story['error'] = array(
+			'pretty' => 'Could not get zip file',
+			'response' => $response
+		);
 	}
+
 	return $story;
 }
 
