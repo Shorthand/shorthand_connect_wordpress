7b8ef511e1861ed34f6c37edbf2affc038f26c32
bradly-shorthand <bradly@shorthand.com>
2024-03-11 14:12:30 +0000

    Updates Shorthand Plugin
    Uses Fixes from TES + Our own fixes to ensure contents meet VIP WP Standards.
    
    Except for the Sanitization of templates + Regex as it's not feasible.

--- src/includes/api.php
+++ src/includes/api.php
@@ -1,184 +1,177 @@
 <?php
-function sh_v2_api_get($url, $options)
-{
-	$token = get_option('sh_v2_token');
-	if (!$token) {
+function sh_v2_api_get( $url, $options ) {
+	$token = get_option( 'sh_v2_token' );
+	if ( ! $token ) {
 		return false;
 	}
 	global $serverURL;
-	$url = $serverURL . $url;
-	$plugin_path = plugin_dir_path( dirname( __FILE__ ) ) . '/shorthand_connect.php';
-	$plugin_data = get_file_data( $plugin_path, array( 'Version' => 'Version' ) );
+	$url            = $serverURL . $url;
+	$plugin_path    = plugin_dir_path( __DIR__ ) . '/shorthand_connect.php';
+	$plugin_data    = get_file_data( $plugin_path, array( 'Version' => 'Version' ) );
 	$plugin_version = $plugin_data['Version'];
-	
+
 	$wp_version = $GLOBALS['wp_version'];
 	$user_agent = 'WordPress/' . $wp_version . ' Shorthand/' . $plugin_version;
-	
+
 	$request_options = array_merge(
 		array(
-			'headers' => array(
+			'headers'       => array(
 				'Authorization' => 'Token ' . $token,
-				'user-agent'  => $user_agent,
+				'user-agent'    => $user_agent,
 			),
-			'http_api_args' => $options
+			'http_api_args' => $options,
 		),
 		$options
 	);
-	
-	if (function_exists("vip_safe_wp_remote_get") && !isset($options['timeout'])){
-		return vip_safe_wp_remote_get($url,false,1,3,10, $request_options);
+
+	if ( function_exists( 'vip_safe_wp_remote_get' ) ) {
+		return vip_safe_wp_remote_get( $url, false, 1, 3, 10, $request_options );
 	} else {
-		return wp_remote_get($url, $request_options);
+		return wp_remote_get( $url, $request_options ); // @codingStandardsIgnoreLine
 	}
-	
 }
 
-function sh_v2_api_get_json($url, $options)
-{
-	$response = sh_v2_api_get($url, $options);
-	$body = wp_remote_retrieve_body($response);
-	return json_decode($body);
+function sh_v2_api_get_json( $url, $options ) {
+	$response = sh_v2_api_get( $url, $options );
+	$body     = wp_remote_retrieve_body( $response );
+	return json_decode( $body );
 }
 
-function sh_get_profile()
-{
+function sh_get_profile() {
 	$tokeninfo = array();
-	
-	$data = sh_v2_api_get_json('/v2/token-info', array());
-	if ($data && isset($data->organisation_id)) {
-		$tokeninfo['username'] = $data->name . ' ('.$data->token_type.' Token)';
+
+	$data = sh_v2_api_get_json( '/v2/token-info', array() );
+	if ( $data && isset( $data->organisation_id ) ) {
+		$tokeninfo['username'] = $data->name . ' (' . $data->token_type . ' Token)';
 		$tokeninfo['gravatar'] = $data->logo;
-		$tokeninfo = (object)$tokeninfo;
+		$tokeninfo             = (object) $tokeninfo;
 	}
-	
+
 	return $tokeninfo;
 }
 
-function sh_get_stories()
-{
+function sh_get_stories() {
 	$stories = null;
-	
-	$data = sh_v2_api_get_json('/v2/stories', array('timeout' => '240'));
-	if ($data) {
+
+	$data = sh_v2_api_get_json( '/v2/stories', array( 'timeout' => '240' ) );
+	if ( $data ) {
 		$stories = array();
-		//Something went wrong
-		if (isset($data->status) && $data->status) {
+		// Something went wrong
+		if ( isset( $data->status ) && $data->status ) {
 			return null;
 		}
-		foreach ($data as $storydata) {
-			$story = array(
-				'image' => $storydata->signedCover,
-				'id' => $storydata->id,
-				'metadata' => (object)array(
-					'description' => $storydata->description
+		foreach ( $data as $storydata ) {
+			$story     = array(
+				'image'         => $storydata->signedCover,
+				'id'            => $storydata->id,
+				'metadata'      => (object) array(
+					'description' => $storydata->description,
 				),
-				'title' => $storydata->title,
-				'story_version' => ''.$storydata->version
+				'title'         => $storydata->title,
+				'story_version' => '' . $storydata->version,
 			);
-			$stories[] = (object)$story;
+			$stories[] = (object) $story;
 		}
 	}
-	
+
 	return $stories;
 }
 
-function sh_get_story_path($post_id, $story_id)
-{
+function sh_get_story_path( $post_id, $story_id ) {
 	init_WP_Filesystem();
-	$destination = wp_upload_dir();
-	$destination_path = $destination['path'].'/shorthand/'.$post_id.'/'.$story_id;
-	
-	if (!file_exists($destination_path)) {
+	$destination      = wp_upload_dir();
+	$destination_path = $destination['path'] . '/shorthand/' . $post_id . '/' . $story_id;
+
+	if ( ! file_exists( $destination_path ) ) {
 		$destination_path = null;
 	}
-	
-	$destination_path = apply_filters('sh_get_story_path', $destination_path, $destination);
-	
+
+	$destination_path = apply_filters( 'sh_get_story_path', $destination_path, $destination );
+
 	return $destination_path;
 }
 
-function sh_get_story_url($post_id, $story_id)
-{
+function shorthand_get_story_url( $post_id, $story_id ) {
 	init_WP_Filesystem();
-	$destination = wp_upload_dir();
-	$destination_url = $destination['url'].'/shorthand/'.$post_id.'/'.$story_id;
-	$destination_url = apply_filters('sh_get_story_url', $destination_url);
-	
+	$destination     = wp_upload_dir();
+	$destination_url = $destination['url'] . '/shorthand/' . $post_id . '/' . $story_id;
+	$destination_url = apply_filters( 'shorthand_get_story_url', $destination_url );
+
 	return $destination_url;
 }
 
-function sh_copy_story($post_id, $story_id, $without_assets=false, $assets_only=false)
-{
-	
-	wp_raise_memory_limit('admin');
+function sh_copy_story( $post_id, $story_id, $without_assets = false, $assets_only = false ) {
+
+	wp_raise_memory_limit( 'admin' );
 	init_WP_Filesystem();
-	$destination = wp_upload_dir();
-	$tmpdir = get_temp_dir();
-	$destination_path = $destination['path'].'/shorthand/'.$post_id;
-	$story = array();
-	
-	//Attempt to connect to the server
-	$zip_file = wp_tempnam('sh_zip',$tmpdir);
-	$response = sh_v2_api_get('/v2/stories/'.$story_id.($without_assets?'?without_assets=true':'').($assets_only?'?assets_only=true':''), array(
-		'timeout' => '600',
-		'stream' => true,
-		'filename' => $zip_file
-	));
-	if (is_wp_error($response)) {
+	$destination      = wp_upload_dir();
+	$tmpdir           = get_temp_dir();
+	$destination_path = $destination['path'] . '/shorthand/' . $post_id;
+	$story            = array();
+
+	// Attempt to connect to the server
+	$zip_file = wp_tempnam( 'sh_zip', $tmpdir );
+	$response = sh_v2_api_get(
+		'/v2/stories/' . $story_id . ( $without_assets ? '?without_assets=true' : '' ) . ( $assets_only ? '?assets_only=true' : '' ),
+		array(
+			'timeout'  => '600',
+			'stream'   => true,
+			'filename' => $zip_file,
+		)
+	);
+	if ( is_wp_error( $response ) ) {
 		$story['error'] = array(
 			'pretty' => ' Request to Shorthand failed',
-			'error' => $response->get_error_message($response)
+			'error'  => $response->get_error_message( $response ),
 		);
-	} elseif (!$response || $response['response']['code'] != 200) {
+	} elseif ( ! $response || $response['response']['code'] != 200 ) {
 		$story['error'] = array(
-			'pretty' => 'Request to Shorthand failed; check the token is configured correctly',
-			'response' => $response
+			'pretty'   => 'Request to Shorthand failed; check the token is configured correctly',
+			'response' => $response,
 		);
 	} else {
-		$story = extractStoryContent($zip_file, $destination_path, $story_id);
+		$story = extractStoryContent( $zip_file, $destination_path, $story_id );
 	}
-	
-	do_action('sh_copy_story', $post_id, $story_id, $story);
-	wp_delete_file($zip_file);
+
+	do_action( 'sh_copy_story', $post_id, $story_id, $story );
+	wp_delete_file( $zip_file );
 	return $story;
 }
 
 
-function init_WP_Filesystem()
-{
+function init_WP_Filesystem() {
 	WP_Filesystem();
 	global $wp_filesystem;
-	if (!is_a($wp_filesystem, 'WP_Filesystem_Base')) {
-		$creds = request_filesystem_credentials(site_url());
-		wp_filesystem($creds);
+	if ( ! is_a( $wp_filesystem, 'WP_Filesystem_Base' ) ) {
+		$creds = request_filesystem_credentials( site_url() );
+		wp_filesystem( $creds );
 	}
 }
 
-function extractStoryContent($zip_file, $destination_path, $story_id)
-{
-	//WP VIP HOSTING COMPATIBILITY
-	$zip = new ZipArchive;
-	if ($zip->open($zip_file)) {
-		wp_mkdir_p($destination_path.'/'.$story_id);
-		$zip->extractTo($destination_path.'/'.$story_id);
+function extractStoryContent( $zip_file, $destination_path, $story_id ) {
+	// WP VIP HOSTING COMPATIBILITY
+	$zip = new ZipArchive();
+	if ( $zip->open( $zip_file ) ) {
+		wp_mkdir_p( $destination_path . '/' . $story_id );
+		$zip->extractTo( $destination_path . '/' . $story_id );
 		$zip->close();
-		if (is_wp_error($zip)) {
+		if ( is_wp_error( $zip ) ) {
 			$story['error'] = array(
-				'pretty' => 'Could not copy story into Wordpress',
-				'error' => $zip->get_error_message(),
-				'unzip error'=> $zip_file
+				'pretty'      => 'Could not copy story into Wordpress',
+				'error'       => $zip->get_error_message(),
+				'unzip error' => $zip_file,
 			);
-			if (function_exists("wp_filesize")){
-				$story['error']['downloaded zip size'] = wp_filesize($zip_file);
+			if ( function_exists( 'wp_filesize' ) ) {
+				$story['error']['downloaded zip size'] = wp_filesize( $zip_file );
 			}
 		} else {
-			$story['path'] = $destination_path.'/'.$story_id;
+			$story['path'] = $destination_path . '/' . $story_id;
 		}
 	} else {
 		$story['error'] = array(
-			'pretty' => 'Could not unzip file'
+			'pretty' => 'Could not unzip file',
 		);
 	}
-	
+
 	return $story;
 }
--- src/includes/shorthand_options.php
+++ src/includes/shorthand_options.php
@@ -26,41 +26,22 @@ $default_sh_site_css = '
 ';
 
 // JSON Checker
-function validate_json($json_string) {
-    // Try to decode the JSON data. If it fails, the JSON is invalid.
-    $json_data = json_decode($json_string, true);
+function validate_json( $json_string ) {
+	// Try to decode the JSON data. If it fails, the JSON is invalid.
+	$json_data = json_decode( $json_string, true );
 
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        // The JSON is invalid.
-        return false;
-    }
+	if ( json_last_error() !== JSON_ERROR_NONE ) {
+		// The JSON is invalid.
+		return false;
+	}
 
-    // Return the original JSON string if it's valid.
-    return $json_string;
+	// Return the original JSON string if it's valid.
+	return $json_string;
 }
 
-function shand_shorthand_options()
-{
+function shand_shorthand_options() {
 	global $default_sh_site_css;
 	global $serverURL;
-
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( esc_html(__( 'You do not have sufficient permissions to access this page.' )) );
-	}
-	if( isset($_POST['sh_submit_hidden']) && $_POST['sh_submit_hidden'] == 'Y' && check_admin_referer( 'sh-update-configuration' ) ) {
-		//If there's a token set, use it, if not set it to an empty string
-		$sh_v2_token = isset($_POST['sh_v2_token']) ? sanitize_text_field($_POST['sh_v2_token']) : '';
-		update_option('sh_v2_token', $sh_v2_token);
-	}
-	
-	$v2_token = esc_html(get_option('sh_v2_token'));
-	
-	if( isset($_POST['sh_submit_hidden_two']) && $_POST['sh_submit_hidden_two'] == 'Y' && check_admin_referer( 'sh-update-configuration' ) ) {
-		//Check if there's custom CSS, if there is, use wp_kses_post() to sanitize otherwise set an empty string
-		$sh_css = isset($_POST['sh_css']) ? wp_kses_post($_POST['sh_css']) : '';
-		update_option('sh_css', $sh_css);
-	}
-
 
 	// Rather than running a rewrite flush everytime a post is submitted, run it on plugin activate/deactivate
 	function shand_rewrite_flush() {
@@ -70,90 +51,100 @@ function shand_shorthand_options()
 	register_activation_hook( __FILE__, 'shand_rewrite_flush' );
 	register_deactivation_hook( __FILE__, 'flush_rewrite_rules' );
 
-	if( isset($_POST['sh_submit_hidden_three']) && $_POST['sh_submit_hidden_three'] == 'Y' && check_admin_referer( 'sh-update-configuration' ) ) {
-		//Check if there's custom permalink, if there is, use sanitize_text_field() to sanitize potential HTML and then set an empty string
-		$sh_permalink = isset($_POST['sh_permalink']) ? sanitize_text_field($_POST['sh_permalink']) : '';
-		update_option('sh_permalink', $sh_permalink);
+	if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die( esc_html( __( 'You do not have sufficient permissions to access this page.' ) ) );
+	}
+	if ( isset( $_POST['sh_submit_hidden'] ) && $_POST['sh_submit_hidden'] == 'Y' && check_admin_referer( 'sh-update-configuration' ) ) {
+		// If there's a token set, use it, if not set it to an empty string
+		$sh_v2_token = isset( $_POST['sh_v2_token'] ) ? sanitize_text_field( $_POST['sh_v2_token'] ) : '';
+		update_option( 'sh_v2_token', $sh_v2_token );
+	}
+
+	$v2_token = esc_html( get_option( 'sh_v2_token' ) );
+
+	if ( isset( $_POST['sh_submit_hidden_two'] ) && $_POST['sh_submit_hidden_two'] == 'Y' && check_admin_referer( 'sh-update-configuration' ) ) {
+		// Check if there's custom CSS, if there is, use wp_kses_post() to sanitize otherwise set an empty string
+		$sh_css = isset( $_POST['sh_css'] ) ? wp_kses_post( $_POST['sh_css'] ) : '';
+		update_option( 'sh_css', $sh_css );
+	}
+
+	if ( isset( $_POST['sh_submit_hidden_three'] ) && $_POST['sh_submit_hidden_three'] == 'Y' && check_admin_referer( 'sh-update-configuration' ) ) {
+		// Check if there's custom permalink, if there is, use sanitize_text_field() to sanitize potential HTML and then set an empty string
+		$sh_permalink = isset( $_POST['sh_permalink'] ) ? sanitize_text_field( $_POST['sh_permalink'] ) : '';
+		update_option( 'sh_permalink', $sh_permalink );
 		shand_rewrite_flush();
 	}
-	$permalink_structure = esc_html(get_option('sh_permalink'));
+	$permalink_structure = esc_html( get_option( 'sh_permalink' ) );
 
-	if ($permalink_structure == '') {
-		update_option('sh_permalink', 'shorthand_story');
-		$permalink_structure = esc_html(get_option('sh_permalink'));
+	if ( $permalink_structure == '' ) {
+		update_option( 'sh_permalink', 'shorthand_story' );
+		$permalink_structure = esc_html( get_option( 'sh_permalink' ) );
 	}
-	$sh_css = get_option('sh_css');
+	$sh_css = get_option( 'sh_css' );
 	$no_css = false;
-	if ($sh_css == '') {
+	if ( $sh_css == '' ) {
 		$no_css = true;
-		if (isset($default_site_css)){
-			update_option('sh_css', $default_site_css);
+		if ( isset( $default_site_css ) ) {
+			update_option( 'sh_css', $default_site_css );
 		}
 		$sh_css = $default_sh_site_css;
 	}
 
-if (isset($_POST['sh_submit_hidden_four']) && $_POST['sh_submit_hidden_four'] == 'Y' && check_admin_referer('sh-update-configuration')) {
-    $sh_regex_list = isset($_POST['sh_regex_list']) ? wp_unslash($_POST['sh_regex_list']) : '';
+	if ( isset( $_POST['sh_submit_hidden_four'] ) && $_POST['sh_submit_hidden_four'] == 'Y' && check_admin_referer( 'sh-update-configuration' ) ) {
+		$sh_regex_list = isset( $_POST['sh_regex_list'] ) ? wp_unslash( $_POST['sh_regex_list'] ) : '';
 
-    if (empty($sh_regex_list)) {
-        // Update the option with an empty value if the input is empty
-        update_option('sh_regex_list', '');
-    } else {
-        // Validate if it's a valid JSON without sanitizing
-        $sh_regex_list = validate_json($sh_regex_list);
+		if ( empty( $sh_regex_list ) ) {
+			// Update the option with an empty value if the input is empty
+			update_option( 'sh_regex_list', '' );
+		} else {
+			// Validate if it's a valid JSON without sanitizing
+			$sh_regex_list = validate_json( $sh_regex_list );
 
-        if ($sh_regex_list !== false) {
-            // Since we are storing it as base64, no need to sanitize the JSON, as base64_encode will handle that
-            update_option('sh_regex_list', base64_encode($sh_regex_list));
-        } else {
-            // Handle invalid JSON error here.
-        }
-    }
-}
+			if ( $sh_regex_list !== false ) {
+				// Since $sh_regex_list stored as base64, no need to sanitize the JSON, as base64_encode will handle that
+				update_option( 'sh_regex_list', base64_encode( $sh_regex_list ) );
+			} else {
+				// Handle invalid JSON error here.
+			}
+		}
+	}
 
-
-
-	$sh_regex_list = base64_decode(get_option('sh_regex_list'));
+	$sh_regex_list = base64_decode( get_option( 'sh_regex_list' ) );
 
 	// Experimental Settings
-	if (isset($_POST['sh_submit_hidden_experimental']) && $_POST['sh_submit_hidden_experimental'] == 'Y' && check_admin_referer('sh-update-configuration')) {
-		$sh_media_cron_offload = isset($_POST['sh_media_cron_offload']) ? filter_var($_POST['sh_media_cron_offload'], FILTER_VALIDATE_BOOLEAN) : false;
-		$sh_disable_acf = isset($_POST['sh_disable_acf']) ? filter_var($_POST['sh_disable_acf'], FILTER_VALIDATE_BOOLEAN) : false;
-		update_option('sh_media_cron_offload', $sh_media_cron_offload);
-		update_option('sh_disable_acf', $sh_disable_acf);
+	if ( isset( $_POST['sh_submit_hidden_experimental'] ) && $_POST['sh_submit_hidden_experimental'] == 'Y' && check_admin_referer( 'sh-update-configuration' ) ) {
+		$sh_media_cron_offload = isset( $_POST['sh_media_cron_offload'] ) ? filter_var( $_POST['sh_media_cron_offload'], FILTER_VALIDATE_BOOLEAN ) : false;
+		$sh_disable_acf        = isset( $_POST['sh_disable_acf'] ) ? filter_var( $_POST['sh_disable_acf'], FILTER_VALIDATE_BOOLEAN ) : false;
+		update_option( 'sh_media_cron_offload', $sh_media_cron_offload );
+		update_option( 'sh_disable_acf', $sh_disable_acf );
 	}
-  $sh_media_cron_offload = filter_var(get_option('sh_media_cron_offload'), FILTER_VALIDATE_BOOLEAN);
-  $sh_disable_acf = filter_var(get_option('sh_disable_acf'), FILTER_VALIDATE_BOOLEAN);
-  
+	$sh_media_cron_offload = filter_var( get_option( 'sh_media_cron_offload' ), FILTER_VALIDATE_BOOLEAN );
+	$sh_disable_acf        = filter_var( get_option( 'sh_disable_acf' ), FILTER_VALIDATE_BOOLEAN );
+
 	$profile = sh_get_profile();
-
-	ob_start();
-	wp_nonce_field( 'sh-update-configuration' );
-	$n_once  = ob_get_clean();
-
-?>	
+	?>	
 <div class="container">
 	<div class="py-1">
 	<h1>Shorthand API Configuration</h1>
 	<h2>Shorthand Connect Status</h2>
-	<form name="form1" method="post">
-		<?php echo $n_once ?>
+	<form name="formtoken" method="post">
+		<?php wp_nonce_field( 'sh-update-configuration' ); ?>
 		<input type="hidden" name="sh_submit_hidden" value="Y" />
 		<table class="form-table"><tbody>
 		<tr class="v2row">
-			<th scope="row"><label for="sh_v2_token"><?php esc_html_e("Shorthand Team Token", 'sh-v2-token' ); ?></label></th>
-			<td><input type="text" id="sh_v2_token" name="sh_v2_token" value="<?php echo esc_attr($v2_token); ?>" size="28"></td>
+		<th scope="row"><label for="sh_v2_token"><?php esc_html_e( 'Shorthand Team Token', 'sh-v2-token' ); ?></label></th>
+		<td><input type="text" id="sh_v2_token" name="sh_v2_token" value="<?php echo esc_attr( $v2_token ); ?>" size="28"></td>
 		</tr>
 		</tbody></table>
-		<?php if ($profile) { ?>
+		<?php if ( $profile ) { ?>
 		<p class="status">Successfully connected</p>
-		<p><strong>Username</strong>: <?php echo esc_html($profile->username); ?></p>
+		<p><strong>Username</strong>: <?php echo esc_html( $profile->username ); ?></p>
 	<?php } else { ?>
 		<p class="status warn">Not Connected</p>
 	<?php } ?>
 	<div style='clear:both'></div>
 		<p class="submit">
-			<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+		<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes' ); ?>" />
 		</p>
 	</form>
 	</div>
@@ -163,13 +154,13 @@ if (isset($_POST['sh_submit_hidden_four']) && $_POST['sh_submit_hidden_four'] ==
 	<h2>Shorthand Permalink Structure</h2>
 		<p>Use this to set the permalink structure of Shorthand story URLs</p>
 		<form name="permalinks" method="post">
-			<?php echo $n_once ?>
+			<?php wp_nonce_field( 'sh-update-configuration' ); ?>
 			<input type="hidden" name="sh_submit_hidden_three" value="Y" />
 			<p>
-				<?php _e("Permalink structure:", 'sh-permalink-value' ); ?><br /> <?php echo get_site_url(); ?>/<input type="text" name="sh_permalink" value="<?php echo esc_attr($permalink_structure); ?>" size="20">/{STORY_NAME}
+				<?php _e( 'Permalink structure:', 'sh-permalink-value' ); ?><br /><?php echo esc_url( get_site_url() ); ?>/<input type="text" name="sh_permalink" value="<?php echo esc_attr( $permalink_structure ); ?>" size="20">/{STORY_NAME}
 			</p>
 			<p class="submit">
-				<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+				<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes' ); ?>" />
 			</p>
 		</form>
 	</div>
@@ -177,15 +168,15 @@ if (isset($_POST['sh_submit_hidden_four']) && $_POST['sh_submit_hidden_four'] ==
 	<div class="py-1">
 	<h2>Shorthand Story Page CSS (theme wide CSS)</h2>
 		<p>Use this CSS to customise Shorthand Story pages to better suit your theme</p>
-		<?php if ($no_css) { ?>
+		<?php if ( $no_css ) { ?>
 			<p class="status warn">No custom CSS found, using default theme CSS</p>
-		<?php }?>
+		<?php } ?>
 		<form name="themecss" method="post">
-			<?php echo $n_once ?>
+			<?php wp_nonce_field( 'sh-update-configuration' ); ?>
 			<input type="hidden" name="sh_submit_hidden_two" value="Y" />
-			<textarea rows="10" cols="80" name="sh_css"><?php echo esc_textarea($sh_css); ?></textarea>
+			<textarea rows="10" cols="80" name="sh_css"><?php echo esc_textarea( $sh_css ); ?></textarea>
 			<p class="submit">
-				<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+				<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes' ); ?>" />
 			</p>
 		</form>
 		</div>
@@ -196,37 +187,37 @@ if (isset($_POST['sh_submit_hidden_four']) && $_POST['sh_submit_hidden_four'] ==
 		<p><em>This Example removes title tags from within the head tag by replacing it with nothing.</em></p>
 <pre><code>
 {
-  "head": [
-    {
-      &quot;query&quot;: &quot;/&lt;title&gt;(.*?)&lt;\\/title&gt;/&quot;,
-      &quot;replace&quot;: &quot;&quot;
-    }
-  ],
-  "body": []
+	"head": [
+	{
+		&quot;query&quot;: &quot;/&lt;title&gt;(.*?)&lt;\\/title&gt;/&quot;,
+		&quot;replace&quot;: &quot;&quot;
+	}
+	],
+	"body": []
 }
 
 </code></pre>
 		<form name="postprocessing" method="post">
-			<?php echo $n_once ?>
+			<?php wp_nonce_field( 'sh-update-configuration' ); ?>
 			<input type="hidden" name="sh_submit_hidden_four" value="Y" />
-			<textarea rows="10" cols="80" id="sh_regex_list" name="sh_regex_list"><?php echo stripslashes($sh_regex_list); ?></textarea>
+			<textarea rows="10" cols="80" id="sh_regex_list" name="sh_regex_list"><?php echo esc_textarea( $sh_regex_list ); ?></textarea>
 			<p class="submit">
-				<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+				<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes' ); ?>" />
 			</p>
 		</form>
 		<script>
 			let textarea = document.querySelector("textarea#sh_regex_list");
 		
 			textarea.addEventListener("keyup", function(event) {
-				try{
+				try {
 					JSON.parse(textarea.value);
 					textarea.setCustomValidity("");
 					
-				}catch(err){
+				} catch(err) {
 					if(textarea.value != ""){
 						console.log("Invalid JSON");
 						textarea.setCustomValidity("Invalid JSON in the Post-processing field");
-					}else{
+					} else {
 						textarea.setCustomValidity("");
 					}
 				}
@@ -234,29 +225,34 @@ if (isset($_POST['sh_submit_hidden_four']) && $_POST['sh_submit_hidden_four'] ==
 			});
 		</script>
 	</div>
-	
+
 	<div class="py-1">
-		<h2>Experimental Features</h2>
-		<p>Early access features that are still subject to change.</p>
-		<form name="form_experimental" method="post">
-		<?php echo $n_once ?>
-		<input type="hidden" name="sh_submit_hidden_experimental" value="Y" />
-		<input type="checkbox" id="sh_media_cron_offload" name="sh_media_cron_offload" value="true" <?php echo esc_attr($sh_media_cron_offload ? 'checked' : '') ?> />
+	<h2>Experimental Features</h2>
+	<p>Early access features that are still subject to change.</p>
+	<form name="form_experimental" method="post">
+			<?php wp_nonce_field( 'sh-update-configuration' ); ?>
+		<input type="hidden" name="sh_submit_hidden_experimental" value="Y"/>
+		<input type="checkbox" id="sh_media_cron_offload" name="sh_media_cron_offload"
+			value="true" <?php echo esc_attr( $sh_media_cron_offload ? 'checked' : '' ); ?> />
 		<label for="sh_media_cron_offload">Import media assets via cron</label>
-		<p>Assets will be fetched after story save to prevent potential execution timeouts. Media won't be immediately available on save but progress will be updated based on the `media_status` field.</p>
-		<p>It is advised that Shorthand Story Posts are saved as a draft first to trigger the cron job prior to public publishing.</p>
+		<p>Assets will be fetched after story save to prevent potential execution timeouts. Media won't be immediately
+		available on save but progress will be updated based on the `media_status` field.</p>
+		<p>It is advised that Shorthand Story Posts are saved as a draft first to trigger the cron job prior to public
+		publishing.</p>
 		<br/>
-		<input type="checkbox" id="sh_disable_acf" name="sh_disable_acf" value="true" <?php echo esc_attr($sh_disable_acf ? 'checked' : '') ?> />
+		<input type="checkbox" id="sh_disable_acf" name="sh_disable_acf"
+			value="true" <?php echo esc_attr( $sh_disable_acf ? 'checked' : '' ); ?> />
 		<label for="sh_disable_acf">Disable Advanced Custom Fields</label>
-		<p>Used to prevent any potential issues that could cause the Shorthand Custom Fields to become hidden by Advanced Custom Fields.</p>
+		<p>Used to prevent any potential issues that could cause the Shorthand Custom Fields to become hidden by Advanced
+		Custom Fields.</p>
 		</br>
 		<p class="submit">
-		<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+		<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes' ); ?>"/>
 		</p>
-		</form>
-		</div>
+	</form>
 	</div>
-		</div>
+</div>
+	</div>
 <style>
 		.py-1 {
 			padding: 1em;
@@ -297,7 +293,7 @@ if (isset($_POST['sh_submit_hidden_four']) && $_POST['sh_submit_hidden_four'] ==
 	</style>
 
 
-<?php
+	<?php
 }
 
 add_action( 'admin_menu', 'shand_shorthand_menu' );
