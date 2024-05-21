7676c5c2b6c3bd88d19466ba20fa1c7a1787e96f
bradly-shorthand <bradly@shorthand.com>
2024-05-21 20:58:15 +0100

    Updated server_url
    Changed server_url to point to live api rather than test server

--- src/config.default.php
+++ src/config.default.php
@@ -4,7 +4,7 @@ global $server_url;
 global $show_archived_stories;
 global $noabstract;
 
-$server_url           = 'https://test-pla-1508-wpvip-api.test.shorthand.com';
+$server_url           = 'https://api.shorthand.com';
 $show_archived_stories = false;
 
 if ( defined( 'SHORTHAND_NOABSTRACT' ) ) {
